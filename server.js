a82e5610c176a27c0470ff08967cbb3cbe0d32db
raomubasher5 <raomubasher55@gmail.com>
2025-06-03 02:37:08 +0500

    add cors

--- server.js
+++ server.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
   'http://localhost:3001',
   'http://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://lms-project-zahid.netlify.app'
+  'https://lms-project-zahid.netlify.app',
+  'https://lms.cosha.eu'
 ];
 
 
