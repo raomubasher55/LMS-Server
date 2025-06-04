50b26c90794ef96f61857d08caa8029208616356
raomubasher5 <raomubasher55@gmail.com>
2025-06-04 14:11:51 +0500

    use wss

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://127.0.0.1:3000',
   'https://lms-project-zahid.netlify.app',
-  'https://lms.cosha.eu'
+  'https://lms.cosha.eu',
+  'ws://lms-server.cosha.eu'
 ];
 
 
